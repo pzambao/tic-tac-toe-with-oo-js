7b1ab522b652950f19cf1f7c0547fc809ee52250
Pedro Zambão <pedrozambaoo@gmail.com>
2021-08-16 13:18:57 -0300

    added name to players

--- script.js
+++ script.js
@@ -1,63 +1,93 @@
+// variáveis globais essenciais para o sistema de encerramento de partida
 var vez = 0;
 var vencedor = false;
 var velha = false;
 
+//Inicia a função JogoDaVelha
 const jogoDaVelha = new JogoDaVelha();
 jogoDaVelha.init();
 
 function JogoDaVelha() {
+  //declaração dos atributos(caracteristica)
+  const jogadores = new Jogadores();
   const tabuleiro = new Tabuleiro();
   const jogador1 = new Jogador1(tabuleiro);
   const jogador2 = new Jogador2(tabuleiro);
   
-
   this.init = function() {
-    const config = { childList: true }; 
-    const observer = new MutationObserver(() => mudaVez());
-    tabuleiro.espacos.forEach((el) => 
-    observer.observe(el, config)); 
-    mudaVez();
+    const config = { childList: true }; // armazena em config qual o tipo de monitoramento do mutation observer
+    const observer = new MutationObserver(() => mudaVez()); // chama em observer a função Mutation Observer e passa quem será monitorado
+    tabuleiro.espacos.forEach((el) => // mapeia todas as divs com a classe ".col" 
+    observer.observe(el, config)); // define el como "responsável" pelo monitoramento e avisa qual tipo de monitoramento.
+    mudaVez(); // chama a função mudaVez
   }
 
+  //declaração do método(ação)
   function mudaVez(){
   vencedor = false;
   velha = false;
-  
-    if (tabuleiro.procuraVencedor()){
+    if (tabuleiro.procuraVencedor()){ // sempre que mudar a vez ele chama o procuraVencedor
       return;
     }
-    if (vez % 2 == 0) {
-      jogador1.mudaVez();
-    } else {
+    if (vez % 2 == 0) { // se a vez dividir por dois e o resto for 0 (par)
+      jogador1.mudaVez();      
+    } else { // caso contrário (impar)
       jogador2.mudaVez();
     }
-
-    vez++;
     
-    if ((vez == 10) && (vencedor == false)){
+    if ((vez == 9) && (vencedor == false)){ // se a vez for 9 e não houver um vencedor, significa que deu velha
       velha = true;
-      let t1 = parseInt(document.querySelector("#empata").innerText);
-      const t2 = parseInt(1)
-      document.querySelector("#empata").innerText = t1 + t2
-      setTimeout( swal({
+      document.querySelector("#empata").innerText++; // lógica que acrescenta +1 ao placa da velha
+      swal({ // alternativa ao alert através da lib "SweetAlert"
         title: "Ihhh, deu velha!",
         text: "Deseja começar uma nova partida?",
         buttons: true,
         dangerMode: true
-      }).then((ok) => {
-        if(ok){
-          vez = 0;
-          for(var i = 1; i <= 9; i++){
-          document.getElementById('posicao' + i).innerHTML = ''; }
+      }).then((ok) => { 
+        if(ok){ // caso selecione começar uma nova partida
+          vez = 0; // define a vez como 0
+          for(var i = 1; i <= 9; i++){ // um for percorrerá todos os espaços do tabuleiro
+            // reseta o conteudo das posições percorridas através do ID (X e O no caso)
+            document.getElementById('posicao' + i).innerHTML = ''; 
+          }
         }
-      }), 130);
+      })
     }
+
+    vez++; // adiciona +1 a vez para que o jogo possa continuar
   }
 }
 
+function Jogadores(){ // função que recebe o nome dos jogadores
+  this.jogX = '';
+  this.jogO = '';
+  swal("Digite o nome do Jogador 1 (X):", {
+    content: "input",
+  })
+  .then((value) => {
+    jogX = value;
+    if(value == '' || value == null ){ // caso não receba valor será definido como padrão "Jogador1 (X)"
+      document.querySelector("#vezX").innerHTML = "<u>Jogador1 (X)</u>";
+    }else{
+      document.querySelector("#vezX").innerHTML = "<u>"+ jogX + " (X)</u>";
+    }
+    swal("Digite o nome do Jogador 2 (O):", {
+      content: "input",
+    })
+    .then((value) => {
+      jogO = value;
+      if(value == '' || value == null ){ // caso não receba valor será definido como padrão "Jogador2 (O)"
+        document.querySelector("#vezO").innerHTML = "Jogador2 (X)";
+      }else{
+        document.querySelector("#vezO").innerHTML = jogO + " (O)";
+      }
+    });
+  });
+}
+
 function Tabuleiro(){  
-  this.espacos = Array.from(document.querySelectorAll('.col'));
-  this.procuraVencedor = function(){
+  this.espacos = Array.from(document.querySelectorAll('.col')); // seleciona todas as classes ".col"
+  this.procuraVencedor = function(){ // armazena todas as combinações vencedoras
     const combinacoesVencedor = [
       [0, 1, 2],    
       [3, 4, 5],
@@ -69,63 +99,64 @@ function Tabuleiro(){
       [2, 5, 8],
     ];
 
-    combinacoesVencedor.forEach((comboVencedor) => {
-      const ehComboVencedor =    
+    combinacoesVencedor.forEach((comboVencedor) => { // percorre todas as posições jogadas buscando se há um combo de posições vencedor.
+      const ehComboVencedor =   
       this.espacos[comboVencedor[0]].innerText !== '' &&
       this.espacos[comboVencedor[0]].innerText == this.espacos[comboVencedor[1]].innerText &&
       this.espacos[comboVencedor[1]].innerText == this.espacos[comboVencedor[2]].innerText;
 
-      if ((ehComboVencedor) && (vencedor == false)){
-        vencedor = true;
-        const jogadorVencedor = this.espacos[comboVencedor[0]].innerText; 
-        let g1 = parseInt(document.querySelector("#vence" + jogadorVencedor).innerText);
-        const g2 = parseInt(1);
-        document.querySelector("#vence" + jogadorVencedor).innerText = g1 + g2;
+      if ((ehComboVencedor) && (vencedor == false)){ // verifica se há o combo vencedor e confirma que a var vencedor está definida como false
+        vencedor = true; // define vencedor como true
+        const jogadorVencedor = this.espacos[comboVencedor[0]].innerText; //armazena 1º valor encontrado no combo vencedor para identificar o vencedor
+        document.querySelector("#vence" + jogadorVencedor).innerText++; // lógica para funcionar o placar
 
-        setTimeout( swal({
-          title: "O jogador " + jogadorVencedor + " venceu!",
+        swal({ // alternativa para o alert através da lib "SweetAlert"
+          title: document.querySelector("#vez" + jogadorVencedor).innerText + " venceu!",
           text: "Deseja começar uma nova partida?",
           buttons: true,
           dangerMode: true
-        }).then((ok) => {
-          if(ok){
-            vez = 0;
-            for(var i = 1; i <= 9; i++){
-            document.getElementById('posicao' + i).innerHTML = '';}
+        }).then((ok) => { 
+          if(ok){ // ao selecionar para começar uma nova partida
+            vez = 0; // a vez irá ser setada como 0 novamente
+            for(var i = 1; i <= 9; i++){ // um for percorrerá todos os espaços do tabuleiro
+              // reseta o conteudo das posições percorridas através do ID (X e O no caso)
+              document.getElementById('posicao' + i).innerHTML = '';
+            } 
           } 
-        }), 130);
-      
+        });
       }    
     });
   };
 }
 
 function Jogador1(tabuleiro) {
-  this.mudaVez = function(index){
-    console.log(vez)
-    tabuleiro.espacos.forEach(el => el.addEventListener('click', digitaVez))
+  this.mudaVez = function(){ 
+    // console.log(vez)
+    // através do monitoramento do mutationObserver, ele detecta a div que foi clicada e chama a função digitaVez
+    tabuleiro.espacos.forEach(el => el.addEventListener('click', digitaVez)) 
   }
-  function digitaVez(event){
-    if(event.target.innerText == ''){
-      event.target.innerText = 'X';
-      tabuleiro.espacos.forEach(el => el.removeEventListener('click', digitaVez))
-      document.querySelector("#vezO").innerHTML = '<u>Jogador 2 (O)</u>'
-      document.querySelector("#vezX").innerHTML = 'Jogador 1 (X)'
+  function digitaVez(event){ // função que insere a jogada na div em questão
+    if(event.target.innerText == ''){ // verifica se já não existe valor dentro da div
+      event.target.innerText = 'X'; // insere através do innerText o X na posição
+      tabuleiro.espacos.forEach(el => el.removeEventListener('click', digitaVez)); // remove a possibilidade do jogador X clicar novamente 
+      document.querySelector("#vezO").innerHTML = '<u>'+ document.querySelector("#vezO").innerHTML +'</u>' // sublinha o jogador O indicando que próximo jogador é ele
+      document.querySelector("#vezX").innerText = document.querySelector("#vezX").innerText // remove o sublinhado de sua vez
     }
   }
 }
 
 function Jogador2(tabuleiro) {
   this.mudaVez = function(){
-    console.log(vez)
+    // console.log(vez)
+    // através do monitoramento do mutationObserver, ele detecta a div que foi clicada e chama a função digitaVez
     tabuleiro.espacos.forEach(el => el.addEventListener('click', digitaVez2))
   }
-  function digitaVez2(event){
-    if(event.target.innerText == ''){
-      event.target.innerText = 'O';
-      tabuleiro.espacos.forEach(el => el.removeEventListener('click', digitaVez2))
-      document.querySelector("#vezO").innerHTML = 'Jogador 2 (O)'
-      document.querySelector("#vezX").innerHTML = '<u>Jogador 1 (X)</u>'
+  function digitaVez2(event){ // função que insere a jogada na div em questão
+    if(event.target.innerText == ''){ // verifica se já não existe valor dentro da div
+      event.target.innerText = 'O'; // insere através do innerText o X na posição
+      tabuleiro.espacos.forEach(el => el.removeEventListener('click', digitaVez2)) // remove a possibilidade do jogador O clicar novamente 
+      document.querySelector("#vezX").innerHTML = '<u>'+ document.querySelector("#vezX").innerHTML +'</u>' // sublinha o jogador X indicando que próximo jogador é ele
+      document.querySelector("#vezO").innerText = document.querySelector("#vezO").innerText // remove o sublinhado de sua vez
     }
   }
 }
